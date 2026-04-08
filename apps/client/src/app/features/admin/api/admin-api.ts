7cc4375e9cf954f207111d4291257636517f8c7d
Michael Berger <m.gonser@icloud.com>
2026-04-08 08:03:12 +0200

    examples

--- apps/client/src/app/features/admin/api/admin-api.ts
+++ apps/client/src/app/features/admin/api/admin-api.ts
@@ -1,4 +1,6 @@
 import { adminEditStore } from '../feat-admin-edit/data/admin-edit-store';
+import { showAdminComponent } from '../feat-admin-edit/ui/show-admin.component';
+import { FeatAdminEditContainerComponent } from '../feat-admin-edit/feat-admin-edit-container.component';
 
 export const adminApi = true;
 
